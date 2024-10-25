f24d181494df9f336f23a7a8fdc3e546d258cccf
Irina Nikitina <915202@gmail.com>
2024-10-25 11:39:09 +0300

    irina_1

---
+++ arena_game.py
@@ -0,0 +1,22 @@
+class Thing:
+    """Класс вещь для усиления персонажей."""
+    def __init__(self):
+        self.name
+        self.protection
+        self.attack
+        self.life
+
+
+brone_protection = Thing(name='Brone_protection',
+                         protection=0.1, attack=0, life=1)
+magic_ring = Thing(name='Magic_ring', protection=0.04,
+                   attack=2, life=0)
+invisible_coat = Thing(name='Invisible_coat', protection=0.1,
+                       attack=0, life=1)
+gun = Thing(name='gun', protection=0, attack=1, life=0)
+laser_automat = Thing(name='laser_automat', protection=0.01,
+                      attack=2, life=0)
+magic_helmet = Thing(name='Magic_helmet', protection=0.08,
+                     attack=0, life=1)
+shining_bomb = Thing(name='shining_bomb', protection=0.01,
+                     attack=3, life=0)
